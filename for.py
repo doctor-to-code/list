09d24261539ad22f2ade87abba1a04c1db9bab9d
Peter Yang <fgdeeerry@gmail.com>
2020-12-28 14:57:12 +0800

    for

---
+++ for.py
@@ -0,0 +1,14 @@
+cars = ['bmw', 'benz']
+for car in cars:
+	print(car)
+
+fruits = ['apple', 'orange', 'watermelon']
+for fruit in fruits:
+	print(fruit)
+
+students = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
+for student in students:
+	print('hi', student)
+for c in students:
+	print(c)
+print('Allen' in c)
